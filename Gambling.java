4da954e8873e6a1babd1c9af22e5b317311271a8
naveeny061 <naveeny061@gmail.com>
2020-12-03 11:01:29 +0530

    Added: Calculative Gambling method to stop when 50% loose or win

--- Gambling.java
+++ Gambling.java
@@ -1,27 +1,37 @@
 import java.util.Random;
 
 public class Gambling {
+
 	public int MakeABet() {
-	Random random=new Random();
-	int result= random.nextInt(2);
-	if(result==1) { 
-		System.out.println("Win");
-		return result;
+		Random random = new Random();
+		int result = random.nextInt(2);
+		if (result == 1) {
+			//System.out.println("Win");
+			return result;
+		} else {
+			//System.out.println("Loose");
+			return result - 1;
+		}
 	}
-	else {
-		System.out.println("Loose");
-		return result-1;
-	}
+	public void Calculative_Gambler() {
+		int totalStack = 100;
+		while (true) {
+			int resultBet = MakeABet();
+			totalStack+=resultBet;
+			if (totalStack == 150 || totalStack == 50) 
+				break;
 			
+		}
+		System.out.println(totalStack);
 	}
-	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulator Problem");
-		int startStake =100;
-		int bet=1;
-		Gambling gamble =new Gambling();
-		int betResult=gamble.MakeABet();
-		int newStack=betResult+startStake;
-		System.out.println(newStack);
+		int startStake = 100;
+		int bet = 1;
+		Gambling gamble = new Gambling();
+		int betResult = gamble.MakeABet();
+		int newStack = betResult + startStake;
+		//System.out.println(newStack);
+		gamble.Calculative_Gambler();
 	}
 }
